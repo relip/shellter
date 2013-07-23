f71e513965895dadce390cae019e889abd529f55
Cloud Kang <secretdog1@gmail.com>
2013-07-23 17:11:19 +0900

    Login module update
    while문 제거.

--- GUI/src/login.cpp
+++ GUI/src/login.cpp
@@ -5,33 +5,17 @@ Login::Login() {
 }
 
 void Login::start() {
-	char input;
-	int i = 0;
-
 	printw("Login\n");
 	
 	printw("ID: ");
 	refresh();
 	getstr(id);
-	printw("%s\n", id);
-	refresh();
 	
 	printw("Password: ");
 	refresh();
 	noecho();
-	while((input = getch()) != '\n') {
-		if(input == KEY_BACKSPACE || input == KEY_DC || input == 127) {
-			i--;
-			delch();
-			pw[i] = NULL;
-		}
-		else {
-			pw[i] = input;
-			i++;
-		}
-	}
-	printw("\n%s\n", pw);
-	refresh();
+	getstr(pw);
+	
 	getch();
 }
 
